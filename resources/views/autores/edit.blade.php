d77acea788f58f2b357a4772f8ce8d28ddf9e83f
jerry martins <jerry@localhost.localdomain>
2018-03-25 21:01:15 -0400

    adiciona dados do autor à view edit

--- resources/views/autores/edit.blade.php
+++ resources/views/autores/edit.blade.php
@@ -6,15 +6,15 @@
         {{ csrf_field() }}
           <div class="form-group">
             <label for="idNome">Nome</label>
-            <input type="text" id="idNome" name="nome" class="form-control" placeholder="nome do autor">
+          <input type="text" id="idNome" name="nome" class="form-control" value="{{$autor->nome}}">
           </div>
           <div class="form-group">
               <label for="idSobreNome">Sobrenome</label>
-              <input type="text" id="idSobreNome" name="sobreNome" class="form-control">
+          <input type="text" id="idSobreNome" name="sobreNome" class="form-control" value="{{$autor->sobreNome}}">
             </div>
           <div class="form-group">
             <label for="idDataNasc">Data Nascimento</label>
-            <input type="date" id="idDataNasc" name="data_nascimento" class="form-control" placeholder="Informe a data de seu nascimento">
+            <input type="date" id="idDataNasc" name="data_nascimento" class="form-control" value="{{$autor->data_nascimento}}">
           </div>
           <button type="submit" class="btn btn-primary">atualizar</button>
         </form>
