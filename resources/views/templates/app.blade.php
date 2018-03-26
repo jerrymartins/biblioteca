84e69cddd89bd4603173ec1a536a21fbf22313e0
jerry martins <jerry@localhost.localdomain>
2018-03-26 00:02:47 -0400

    altera titulo da página

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Biblioteca</title>
   </head>
   <body>
     <!-- Optional JavaScript -->
